1ed6244b324f3c3f1f52bc7043e52c86878028d6
CHUL SEUNG LEE <fpsemfaos@gmail.com>
2025-01-04 07:12:30 -0500

    Implement tweet validator

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import 'express-async-errors';
 import * as tweetController from '../controller/tweet.js';
+import { body } from 'express-validator';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+const validateTwwet = [
+  body('text')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('Text should be at least 3 letters.'),
+  validate,
+];
 
 router.get('/', tweetController.getTweets);
 
 router.get('/:id', tweetController.getTweet);
 
-router.post('/', tweetController.createTweet);
+router.post('/', validateTwwet, tweetController.createTweet);
 
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTwwet, tweetController.updateTweet);
 
 router.delete('/:id', tweetController.deleteTweet);
 
